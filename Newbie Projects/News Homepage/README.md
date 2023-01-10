5913be14bf197655e3e61ba72ef87c3c41b7c8c0
Karthik <skkarthik9819@gmail.com>
2023-01-10 21:53:32 +0530

    finished project

---
+++ Newbie Projects/News Homepage/README.md
@@ -0,0 +1,70 @@
+# Frontend Mentor - News homepage solution
+
+This is a solution to the [News homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+- [Author](#author)
+
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover and focus states for interactive elements
+
+### Screenshots
+
+![Desktop-View](./Screenshots/Desktop%20View.png)
+![Mobile-View](./Screenshots/Mobile%20View.png)
+![Mobile-Menu](./Screenshots/Mobile%20Menu.png)
+![Active-state-1](./Screenshots/Active%20State1.png)
+![Active-state-2](./Screenshots/Active%20State2.png)
+![Active-state-3](./Screenshots/Active%20State3.png)
+
+
+### Links
+
+- Solution URL: [Repo](https://github.com/karthiksk9819/Frontend-Mentor/tree/main/Newbie%20Projects/Product%20preview%20card%20component)
+
+
+## My process
+
+I started with a desktop first apporoach and then did the mobile version.
+ Placed all the compoments and later positioned them using flexbox
+ for the mobile view.
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Sass 
+
+
+### What I learned
+
+Gained an indepth understanding of Grid layout and responsive design.
+
+
+## Author
+
+
+- Frontend Mentor - [profile](https://www.frontendmentor.io/profile/karthiksk9819)
+
+
---
+++ Newbie Projects/News Homepage/index.js
@@ -0,0 +1,68 @@
+let openBtn = document.querySelector(".open");
+let closeBtn = document.querySelector(".close");
+let hero = document.querySelector(".navbar__menu");
+let filter = document.querySelector(".filter");
+let body = document.querySelector("body");
+let main = document.querySelector("main");
+let clicked = false;
+let navbar = document.querySelector(".navbar__menu");
+
+let header = document.querySelector(".header");
+
+console.log(header);
+body.addEventListener("click", (e) => {
+  if (e.target == openBtn) {
+    body.style.overflow = "hidden";
+    toggler();
+
+    header.style.overflow = "auto";
+
+    clicked = true;
+  }
+  if (e.target == closeBtn) {
+    toggler();
+    body.style.overflow = "auto";
+    header.style.overflow = "hidden";
+
+    clicked = false;
+  }
+  if (clicked) {
+    if (e.target != navbar && e.target != openBtn) {
+      toggler();
+
+      header.style.overflow = "hidden";
+    }
+  }
+});
+
+toggler = () => {
+  hero.classList.toggle("hidden");
+  filter.classList.toggle("active");
+  closeBtn.classList.toggle("closed");
+  closeBtn.classList.toggle("opened");
+  openBtn.classList.toggle("opened");
+  openBtn.classList.toggle("closed");
+  clicked = false;
+};
+
+togglerTwo = () => {
+  hero.classList.contains("hidden")
+    ? hero.classList.remove("hidden")
+    : hero.classList.add("hidden");
+
+  closeBtn.classList.contains("opened")
+    ? closeBtn.classList.remove("opened")
+    : closeBtn.classList.add("opened");
+
+  closeBtn.classList.contains("closed")
+    ? closeBtn.classList.remove("closed")
+    : closeBtn.classList.add("closed");
+
+  openBtn.classList.contains("opened")
+    ? openBtn.classList.remove("opened")
+    : openBtn.classList.add("opened");
+
+  openBtn.classList.contains("closed")
+    ? openBtn.classList.remove("closed")
+    : openBtn.classList.add("closed");
+};
